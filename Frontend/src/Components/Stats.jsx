67a5810d1760880aeafe883ca46458d28d24024b
Ayush Kumar <ayushk1708@gmail.com>
2024-05-11 10:52:50 +0530

    Updated

--- Frontend/src/Components/Stats.jsx
+++ Frontend/src/Components/Stats.jsx
@@ -5,7 +5,7 @@ return (
     <div>
     <div className='flex flex-col items-center justify-center background bg-transparent p-3 background'>
         <div className='ml-10 w-[1080px] relative'>
-            <div className='absolute inset-0 backdrop-filter backdrop-blur-sm bg-transparent flex flex-row'></div>
+            <div className='absolute inset-0 backdrop-filter backdrop-blur-md bg-transparent flex flex-row'></div>
             <div className='relative z-10 text-white p-2 items-center text-center'>
                 <div className='text-2xl'>
                     Similar symptoms don't necessarily mean everyone is infected with the same disease.<br/> However, final testings are required for proper medication.
@@ -23,13 +23,13 @@ return (
     </div>
     <div className='flex flex-col items-center justify-center background bg-transparent p-3 background'>
         <div className='ml-10 w-[1080px] relative'>
-            <div className='absolute inset-0 backdrop-filter backdrop-blur-sm bg-transparent flex flex-row'></div>
+            <div className='absolute inset-0 backdrop-filter backdrop-blur-md bg-transparent flex flex-row'></div>
             <div className='relative z-10 text-white p-2 items-center text-center'>
                 <div className='text-2xl'>
-                    Similar symptoms don't necessarily mean everyone is infected with the same disease.<br/> However, final testings are required for proper medication.
+                The image provides information about the number of deaths from pneumonia in 2019 <br/>, categorized by different age groups.
                 </div>
                 <div className='text-2xl'>
-                    Machine learning and interpretability can help doctors make better decisions.
+                This infographic emphasizes the significant impact of pneumonia across various age <br/> ranges, with children under five being particularly affected.
                 </div>
             </div>
             <div className='relative z-10 text-white p-2 items-center text-center mb-3 mt-2'>
@@ -42,14 +42,14 @@ return (
     <div className='flex flex-col items-center justify-center background bg-transparent p-3 background'>
         <div className='ml-10 w-[1080px] relative'>
             <div className='absolute inset-0 backdrop-filter backdrop-blur-sm bg-transparent flex flex-row'></div>
-            <div className='relative z-10 text-white p-2 items-center text-center'>
+            {/* <div className='relative z-10 text-white p-2 items-center text-center'>
                 <div className='text-2xl'>
                     Similar symptoms don't necessarily mean everyone is infected with the same disease.<br/> However, final testings are required for proper medication.
                 </div>
                 <div className='text-2xl'>
                     Machine learning and interpretability can help doctors make better decisions.
                 </div>
-            </div>
+            </div> */}
             <div className='relative z-10 text-white p-2 items-center text-center mb-3 mt-2'>
                 <div className='flex justify-around'>
                     <img className='h-[576px] w-[980px] rounded' src="/img3.jpeg" alt="HealthLens Image" />
